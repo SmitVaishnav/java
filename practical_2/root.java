61e82b05c5f236c1a7ac5a3dda14defcbe192504
SmitVaishnav <smitvaishnav007@gmail.com>
2022-09-01 08:26:10 +0530

    java

---
+++ practical_2/root.java
@@ -0,0 +1,14 @@
+public class root {
+   public static void main(String args[]) {
+       int a =Integer.parseInt(args[0]);
+       int b =Integer.parseInt(args[1]);
+       int c =Integer.parseInt(args[2]);
+
+
+       double root1 = (((-b)+(Math.sqrt((b*b)-(4*a*c))))/(2*a));
+       double root2 = (((-b)-(Math.sqrt((b*b)-(4*a*c))))/(2*a)); 
+
+       System.out.println(root1);
+       System.out.println(root2);
+   } 
+}
